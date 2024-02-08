4156c5739af6afd9075e9dbf06312161f4c6ef6e
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-02-09 00:43:21 +0530

    added filter items option
    users can select perishable or non-perishable fruits from the dropdown

--- fruitList.js
+++ fruitList.js
@@ -9,7 +9,8 @@ function getFruitsData() {
         })
         .then(function (data) {
             createFruitList(data)
-            deleteItemBtn()
+            deleteItemBtn();
+            applyFilter(data)
         })
         .catch(function (error) {
             console.log("Error", error);
@@ -20,14 +21,15 @@ function getFruitsData() {
 getFruitsData()
 
 function createFruitList(data) {
+    unorderedList.innerHTML = '';
 
     for (let i = 0; i < data.length; i++) {
 
         const liElement = document.createElement('li');
         liElement.className = 'list-group-item';
         liElement.innerHTML = `${data[i].name} - Quantity: ${data[i].quantity} - Category: ${data[i].category} <button id="deleteItem" data-id="${data[i]._id}"  class="btn btn-danger d-flex float-end">Delete</button>`
-        unorderedList.appendChild(liElement);
 
+        unorderedList.appendChild(liElement);
     }
 }
 
@@ -61,6 +63,59 @@ function deleteItemBtn() {
 
         })
     }
+}
 
+//-------------------code 1-----------------
+/* 
+const typeFilter = document.querySelector('#typeFilter');
+
+typeFilter.addEventListener('change', applyTypeFilter)
+
+function applyTypeFilter(fruits) {
+
+    const selectedFilter = typeFilter.value
+    console.log(selectedFilter);
+
+    const filteredFruitList = []
+    for (let i = 0; i < fruits.length; i++) {
+        console.log(fruits);
+        if (selectedFilter == fruits[i].category) {
+            filteredFruitList.push(fruits[i]);
+        }
+    }
+    createFruitList(filteredFruitList)
+    console.log(filteredFruitList);
+}
+
+
+ */
+
+//------------------code 2-----------------------
+function applyFilter(data) {
+    const typeFilter = document.querySelector('#typeFilter');
+
+    typeFilter.addEventListener('change', function () {
+
+        let selectedFilter = typeFilter.value;
+
+        const filteredFruitList = [];
+
+
+        if (selectedFilter === "All") {
+            unorderedList.innerHTML = '';
+
+            getFruitsData()
+        } else {
+            for (let i = 0; i < data.length; i++) {
+                if (selectedFilter === data[i].category) {
+                    filteredFruitList.push(data[i])
+                }
+            }
+        }
+        unorderedList.textContent = '';
+        createFruitList(filteredFruitList)
+        console.log(filteredFruitList);
+    })
 
 }
+
